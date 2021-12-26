cb7acd86385a720800bf3aa770a2bc68a391afcc
Maksym Grom <MailMaximGrom@gmail.com>
2021-12-26 18:54:16 +0200

    Attributes Classes Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooComponent } from './components/foo/foo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/foo/foo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.scss']
+})
+export class FooComponent {
+
+}
