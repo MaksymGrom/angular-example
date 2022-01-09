fc684279fa151b98ae7434c6cac09c8177873701
Maksym Grom <MailMaximGrom@gmail.com>
2022-01-09 20:33:39 +0200

    ngAfterViewInit ngAfterViewChecked

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -3,22 +3,29 @@ import {
   AfterContentInit,
   AfterViewChecked,
   AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   DoCheck,
+  ElementRef,
   Input,
   OnChanges,
   OnDestroy,
   OnInit,
-  SimpleChanges
+  QueryList,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren
 } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.scss']
+  styleUrls: ['./parent.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ParentComponent implements
-  OnChanges,
+export class ParentComponent implements OnChanges,
   OnInit,
   DoCheck,
   AfterContentInit,
@@ -27,43 +34,56 @@ export class ParentComponent implements
   AfterViewChecked,
   OnDestroy {
 
+  @ViewChild('jqueryNeverDie', {static: true, read: ElementRef})
+  jqueryContainer!: ElementRef<HTMLDivElement>;
+
+  @ViewChild(ChildComponent)
+  appChild!: ChildComponent;
+
+  @ViewChildren(ChildComponent)
+  appChildren!: QueryList<ChildComponent>;
+
   @Input()
   title = 'before init parent';
 
-  constructor() {
-    console.log('ParentComponent.constructor', this.title);
+  constructor(private cd: ChangeDetectorRef) {
+    console.log('ParentComponent.constructor', this.jqueryContainer);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ParentComponent.ngOnChanges', changes, this.title);
+    console.log('ParentComponent.ngOnChanges', changes, this.jqueryContainer);
   }
 
   ngOnInit(): void {
-    console.log('ParentComponent.ngOnInit', this.title);
+    console.log('ParentComponent.ngOnInit', this.jqueryContainer);
   }
 
   ngDoCheck(): void {
-    console.log('ParentComponent.ngDoCheck', this.title);
+    console.log('ParentComponent.ngDoCheck', this.jqueryContainer);
   }
 
   ngAfterContentInit(): void {
-    console.log('ParentComponent.ngAfterContentInit', this.title);
+    console.log('ParentComponent.ngAfterContentInit', this.jqueryContainer);
   }
 
   ngAfterContentChecked(): void {
-    console.log('ParentComponent.ngAfterContentChecked', this.title);
+    console.log('ParentComponent.ngAfterContentChecked', this.jqueryContainer);
   }
 
   ngAfterViewInit(): void {
-    console.log('ParentComponent.ngAfterViewInit', this.title);
+    // Promise.resolve().then(() => this.appChild.title = 'FOO');
+
+    this.appChild.title = 'FOO';
+    this.cd.detectChanges();
+    console.log('ParentComponent.ngAfterViewInit', this.jqueryContainer);
   }
 
   ngAfterViewChecked(): void {
-    console.log('ParentComponent.ngAfterViewChecked', this.title);
+    console.log('ParentComponent.ngAfterViewChecked', this.jqueryContainer);
   }
 
   ngOnDestroy(): void {
-    console.log('ParentComponent.ngOnDestroy', this.title);
+    console.log('ParentComponent.ngOnDestroy', this.jqueryContainer);
   }
 
 }
