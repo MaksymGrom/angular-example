a0e9234717b7cdf57daecd7172f9c11839a8d88c
MaksymGrom <mailmaximgrom@gmail.com>
2023-08-06 15:30:39 -0400

    how to add styles to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  styles: [
+    `
+      h1 { color: pink; }
+      h2 { color: yellow; }
+    `,
+    `
+      h3 { color: green; }
+    `
+  ]
 })
 export class AppComponent {
   title = 'my-project';
